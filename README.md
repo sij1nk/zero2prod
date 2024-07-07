08a62971debb3cef03624022d1228d16a32f3567
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-07-07 21:39:07 +0200

    3.9

---
+++ README.md
@@ -0,0 +1,3 @@
+# zero2prod
+
+<https://www.zero2prod.com>
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::net::TcpListener;
+
+use zero2prod::configuration::get_configuration;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    zero2prod::startup::run(listener)?.await
+}
